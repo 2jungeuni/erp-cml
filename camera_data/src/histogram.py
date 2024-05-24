87239d31df6acd4db963f1542b89e00be12170f3
Seongjae Lee <leesungjae450@gmail.com>
2024-05-24 20:03:50 +0900

    update code

--- camera_data/src/histogram.py
+++ camera_data/src/histogram.py
@@ -3,17 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
-def gaussian_transform(x, mu, sigma):
-    img_float = x.astype(np.float32)
-    gaussian_pdf = norm.pdf(img_float, mu, sigma)
-    gaussian_pdf = gaussian_pdf / np.max(gaussian_pdf)  # Normalize to range [0, 1]
-    transformed_img = (gaussian_pdf * 255).astype(np.uint8)  # Scale to range [0, 255]
-    return transformed_img
+def gaussian_transform(image, mu, sigma):
+    img_float = image.astype(np.float32)
+    gaussian_img = np.exp(-0.5 * ((img_float - mu) / sigma) ** 2) * 255
+    return gaussian_img.astype(np.uint8)
 # 이미지 로드
 
 image_path = 'a.png'
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-
 x, y = 150, 480
 k = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 dilate = cv2.dilate(image, k, iterations=2)
@@ -22,12 +19,12 @@ cv2.imshow("dilate", dilate)
 max_val = np.min(dilate[y-2:y+3, x-40:x+40])
 dilate_shifted = np.where(dilate == 0, 0, max_val - dilate)
 cv2.imshow("dilate_shifted", dilate_shifted)
-masked_image = np.where((dilate_shifted > 15) & (dilate_shifted < 245), dilate_shifted, 0)
-cv2.imshow("masked_image", masked_image)
+# masked_image = np.where((dilate_shifted > 15) & (dilate_shifted < 245), dilate_shifted, 0)
+# cv2.imshow("masked_image", masked_image)
 
 
 mu, sigma = 170, 50  # 평균과 표준 편차 값 설정
-filtered = gaussian_transform(masked_image, mu, sigma)
+filtered = gaussian_transform(dilate_shifted, mu, sigma)
 cv2.imshow("gaussian", filtered)
 # 히스토그램 계산 및 시각화
 hist = cv2.calcHist([filtered], [0], None, [256], [0, 256])
