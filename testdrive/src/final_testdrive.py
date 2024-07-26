fff93eb632b0a62c9efd94bf5abf03c842855841
sjlee <leesungjae450@gmail.com>
2024-07-26 16:59:39 +0900

    add mpc controller

--- testdrive/src/final_testdrive.py
+++ testdrive/src/final_testdrive.py
@@ -1,68 +1,94 @@
 import rospy
-from std_msgs.msg import UInt8, Bool, Int32
-from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import Point, Twist
+from nav_msgs.msg import Odometry
+from test_drive.msg import ReferencePos
 import numpy as np
+import cvxpy as cp
 
+class MPCController:
+    def __init__(self, horizon=10):
+        rospy.Subscribe("/odom", Odometry, self.odom_update)
+        self.sub = rospy.Subscriber('/waypoints_topic', ReferencePos, self.waypoints_callback)
+        self.pub = rospy.Publisher('/control_command', Point, queue_size=10)
+        self.waypoints = []
+        self.horizon = horizon
+        self.dt = 0.1 # time step
 
-CAR_LENGTH = 105  # 차량의 뒷바퀴 중간부터 앞바퀴 중간까지의 거리 (cm)
+    def waypoints_callback(self, data):
+        self.waypoints = [(point.x, point.y) for point in data.points]
+        self.run_mpc()
 
+    def run_mpc(self):
+        if len(self.waypoints) < self.horizon:
+            return
+        
+        # initial state
+        x0, y0, theta0 = 0.0, 0.0, 0.0 # TODO: change
 
+        # define variables
+        x = cp.Variable(self.horizon)
+        y = cp.Variable(self.horizon)
+        theta = cp.Variable(self.horizon)
+        v = cp.Variable(self.horizon - 1) # linear velocity
+        omega = cp.Variable(self.horizon - 1) # angular velocity
+
+        # define the cost function
+        cost = 0
+        for t in range(self.horizon):
+            cost += cp.square(x[t] - self.waypoints[t][0]) + cp.square(y[t] - self.waypoints[t][1])
+        for t in range(self.horizon - 2):
+            cost += cp.square(v[t] - v[t-1]) + cp.square(omega[t] - omega[t-1]) # control effort
+        
+        # define the constraints
+        constraints = []
+        constraints += [x[0] == x0, y[0]==y0, theta[0]==theta0]
+        for t in range(self.horizon - 1):
+            constraints += [
+                x[t + 1] == x[t] + self.dt * v[t] * cp.cos(theta[t]),
+                y[t + 1] == y[t] + self.dt * v[t] * cp.sin(theta[t]),
+                theta[t + 1] == theta[t] + self.dt * omega[t]
+            ]
+
+        # solve the optimization problem
+        prob = cp.Problem(cp.Minimize(cost), constraints)
+        prob.solve()
+
+        # get the contorl input and publish
+        if prob.status == cp.OPTIMAL:
+            control_cmd = Point()
+            control_cmd.x = v.value[0] # first linear velocity control input
+            control_cmd.y = omega.value[0] # first angular velocity control input
+            control_cmd.z = 0
+            self.pub.publish(Control_cmd)
+''' 
 class ERPtestdrive:
     def __init__(self):
         rospy.Subscriber("/reference_pos", PointStamped, self.testdrive)
-        rospy.Subscriber("/state_e_stop", Bool, self.e_stop_update)
-        rospy.Subscriber("/state_encoder", Int32, self.encoder_update)
-        self.speed_pub = rospy.Publisher("/speed", UInt8, queue_size=3)
-        self.brake_pub = rospy.Publisher("/brake", UInt8, queue_size=3)
-        self.steer_pub = rospy.Publisher("/steer", Int32, queue_size=3)
-        self.e_stop = 0
-        self.encoder = 0
-        self.initial_flag = True
+        rospy.Subscriber("/odom", Odometry, self.odom_update)
+        self.command_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=3)
 
 
-    def e_stop_update(self, data):
+    def odom_update(self, data):
         self.e_stop = data.data
 
 
-    def encoder_update(self, data):  # testing without camera
-        self.encoder = data.data
-        if self.initial_flag:
-            self.initial_encoder = data.data
-            self.initial_flag = False
+    def testdrive(self, data):
+        self.x = data.Point.x
+        self.y = data.Point.y
+        self.z = data.Point.z
 
         
 
-    def testdrive(self, data):
-        brake = 100
-        speed = 0
-        steer = 0
-        if self.e_stop == 0: # if emergency stop is not working
-            # if self.encoder <= self.initial_encoder + 4000: # while car drives about 2.4m
-            brake = 0
-            speed = 20
-            target_x = data.point.x  # 차량의 원점 기준 얼마나 앞의 지점을 기준으로 삼을지
-            delta_y = -1 * data.point.y
-            Ld_power = target_x**2 + delta_y**2
-            steer = np.rad2deg(np.arctan(2 * CAR_LENGTH * delta_y / Ld_power))
-            print("steer(deg): ", steer)
-
-        speed_msg = UInt8()
-        speed_msg.data = speed
-        self.speed_pub.publish(speed_msg)
-        steer_msg = Int32()
-        steer_conv = int(max(-2000, min(2000, round(steer * 71))))
-        # change degree to integer and clipping
-        steer_msg.data = steer_conv
-        self.steer_pub.publish(steer_msg)
-        brake_msg = UInt8()
-        brake_msg.data = brake
-        self.brake_pub.publish(brake_msg)
-
+        command = Twist()
+        command.linear.x = 
+        command.angular.z = 
+        self.brake_pub.publish(command)
         
 
 if __name__ == "__main__":
     rospy.init_node("testdrive")
     node = ERPtestdrive()
-    rate = rospy.Rate(1)
+    rate = rospy.Rate(50)   # 50 Hz
     while not rospy.is_shutdown():
         rate.sleep()
+'''
